cd40eab1da161019ea47efd59bdd339db6574834
nyx47rd <nyx47rd@users.noreply.github.com>
2026-05-19 19:57:20 +0300

    chore: bump version to v2.1.6 [skip release]

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 var autoPlayState = false;
-var CURRENT_VERSION = '2.1.5';
+var CURRENT_VERSION = '2.1.6';
 var updateAvailable = false;
 var latestReleaseUrl = 'https://github.com/nyx47rd/rchelper/releases/latest';
 
