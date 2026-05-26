428faf66e2f200f35cba3228a17ff759845458fa
nyx47rd <nyx47rd@users.noreply.github.com>
2026-05-26 13:57:48 +0300

    chore: bump version to v2.2.50 [skip release]

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-const RC_VERSION = '2.2.49';
+const RC_VERSION = '2.2.50';
 
 /* ── i18n (content script) ── */
 var _RC_LANG_CONTENT = 'tr';
--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 var autoPlayState = false;
-var CURRENT_VERSION = '2.2.49';
+var CURRENT_VERSION = '2.2.50';
 var updateAvailable = false;
 var latestReleaseUrl = 'https://github.com/nyx47rd/rchelper/releases/latest';
 
