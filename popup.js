576a53e0bb9efdbcab5c871a1750740c7fddbb33
Yasar Efe Celik <yasar.123.sevda@gmail.com>
2026-07-06 18:43:10 +0300

    feat: make sync password field visible (type=text) with monospace font

--- popup.js
+++ popup.js
@@ -1,5 +1,5 @@
 var autoPlayState = false;
-var CURRENT_VERSION = '2.2.69';
+var CURRENT_VERSION = '2.2.70';
 var updateAvailable = false;
 var latestReleaseUrl = 'https://github.com/nyx47rd/rchelper/releases/latest';
 
