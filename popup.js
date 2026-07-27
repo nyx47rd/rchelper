23cbd735d7c9455f091e8c96e481f5e9cb0cea73
Yasar Efe Celik <yasar.123.sevda@gmail.com>
2026-07-27 14:22:15 +0300

    chore(release): bump version to 2.3.18 [skip release]

--- popup.js
+++ popup.js
@@ -1,6 +1,6 @@
 var autoPlayState = false;
 var loadSkippedGames = null;
-var CURRENT_VERSION = '2.3.17';
+var CURRENT_VERSION = '2.3.18';
 var updateAvailable = false;
 var latestReleaseUrl = 'https://github.com/nyx47rd/rchelper/releases/latest';
 
